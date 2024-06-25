2907b3905e5e2fb39b4d8be53b5b1273f58c30ec
mario <mario.guriuc@student.uaic.ro>
2024-06-25 14:00:00 +0200

    [Mario] refactor
    Signed-off-by: mario <mario.guriuc@student.uaic.ro>

--- backend/users_service/vendor/phpmailer/phpmailer/src/SMTP.php
+++ backend/users_service/vendor/phpmailer/phpmailer/src/SMTP.php
@@ -1047,7 +1047,7 @@ class SMTP
             //Cut off error code from each response line
             $detail = preg_replace(
                 "/{$code}[ -]" .
-                ($code_ex ? str_replace('.', '\\.', $code_ex) . ' SMTP.php' : '') . '/m',
+                ($code_ex ? str_replace('.', '\\.', $code_ex) . ' ' : '') . '/m',
                 '',
                 $this->last_reply
             );
